5a385d34fdbd3580865b37114b9692868dd4175a
leolanese <laneseleo@gmail.com>
2023-06-15 13:41:40 +0100

    model improvements

--- model/models.js
+++ model/models.js
@@ -1,33 +1,42 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs'
 
-const userSchema = mongoose.Schema({
+// Define User schema
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Please add a name'],
+        trim: true,
+        maxLength: [50, 'Name cannot be more than 50 characters']
     },
     email: {
         type: String,
-        required: true
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 
+            'Please add a valid email'
+        ]
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Please add a password'],
+        minLength: [6, 'Password cannot be less than 6 characters'],
+        select: false // This prevents the password from being returned in a query
     }
-},
-{
-    timestamp: true
-}
-);
+}, {
+    timestamps: true
+});
 
 userSchema.methods.matchPassword = async function (enterendPassword) {
     return await bcrypt.compare(enterendPassword, this.password);
 }
 
 userSchema.pre('save', async function(next){
-    if(!this.isModified('password')){
+    if (!this.isModified('password')){
         next();
     }
+
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 })
