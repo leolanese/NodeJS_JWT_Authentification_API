55bc08604345249f4a165999a03c43d2d22abb32
leolanese <laneseleo@gmail.com>
2023-06-15 13:46:05 +0100

    clonclusions

--- README.md
+++ README.md
@@ -48,13 +48,9 @@ MODE = development
 
 ---
 
-
-
----
-
 ## Connecting to the MongoDb server Cluster
 
-We need Node.js server to rung successfully on port 5050 and it also connected to MongoDB successfully
+We need Node.js server to rung successfully on port 5050 and it also connected to MongoDB successfully. Just to mention, for simplicity MVC pattern will be in place
 
 ## Running server
 
@@ -94,3 +90,14 @@ MongoDB is Connected successfully: ac-okc09tz-shard-00-01.ooedapl.mongodb.net
 > TIP: We need to add Headers: `Key`: `Authorization` & `Value`: `<token-value-here>`
 
 ![Postman API GET valid](./shared/imgs/account.jpg)
+
+---
+### :100: <i>Thanks!</i>
+#### Now, don't be an stranger. Let's stay in touch!
+
+##### :radio_button: linkedin: <a href="https://www.linkedin.com/in/leolanese/" target="_blank">@LeoLanese</a>
+##### :radio_button: Twitter: <a href="https://twitter.com/LeoLanese" target="_blank">@LeoLanese</a>
+##### :radio_button: Portfolio: <a href="https://www.leolanese.com" target="_blank">www.leolanese.com</a>
+##### :radio_button: DEV.to: <a href="https://www.dev.to/leolanese" target="_blank">dev.to/leolanese</a>
+##### :radio_button: Blog: <a href="https://www.leolanese.com/blog" target="_blank">leolanese.com/blog</a>
+##### :radio_button: Questions / Suggestion / Recommendation: developer@leolanese.com
--- controller/controllers.js
+++ controller/controllers.js
@@ -1,62 +1,82 @@
 import asyncHandler from 'express-async-handler';
 import generateJWTToken from '../shared/generateJWTToken.js';
-import UserModel from '../model/models.js'
+import UserModel from '../model/models.js';
 
-const registerUser = asyncHandler( async (req, res) => {
+// Register a new user
+const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
+    const existingUser = await UserModel.findOne({ email });
 
-    const userModel = await UserModel.findOne({email})
-
-    if(userModel){
+    if (existingUser) {
         res.status(400).json({
             success: false,
             message: 'User already exists'
-        })
-    } else{
-        const user = await UserModel.create({ name, email, password})
-        if(user){
+        });
+    } else {
+        const user = await UserModel.create({ name, email, password });
+
+        if (user) {
             res.status(201).json({
                 success: true,
-                message: 'User has created',
-                user: user,
+                message: 'User has been created',
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email
+                },
                 token: generateJWTToken(user._id)
-            })
-        }else{
+            });
+        } else {
             res.status(400).json({
                 success: false,
-                message: 'Something went wrong'
-            })
+                message: 'Error in creating user'
+            });
         }
-    }
-})
-
-
-const loginUser = asyncHandler( async (req, res) => {
-    const { email, password } = req.body;
-
-    const userModel = await UserModel.findOne({email})
-
-    if(userModel && await userModel.matchPassword(password)){
-        res.status(200).json({
-            success: true,
-            message: 'User logged in successfully',
-            email: email,
-            token: generateJWTToken(userModel._id)
-        })
-    } else{
-        res.status(400).json({
-            success: false,
-            message: 'Invalid user/password'
-        })
     }
 });
 
+// User login
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const userModel = await UserModel.findOne({ email });
+
+    if (userModel && await userModel.matchPassword(password)) {
+        res.status(200).json({
+            success: true,
+            message: 'User logged in successfully',
+            user: {
+                id: userModel._id,
+                name: userModel.name,
+                email: userModel.email
+            },
+            token: generateJWTToken(userModel._id)
+        });
+    } else {
+        res.status(401).json({
+            success: false,
+            message: 'Invalid email/password'
+        });
+    }
+});
+
+// Get the private user account info
 const account = asyncHandler(async (req, res) => {
-    res.send('Private Account Section - Registered Users Only')
-})
+    const user = await UserModel.findById(req.user.id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
 
 export {
     registerUser,
     loginUser,
     account
-}
+};
